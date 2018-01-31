cc365bfb2347714a90960500db38bca7f52fde70
andres9722 <andres_9722@outlook.com>
2018-01-31 11:01:14 -0500

    Adding webpack config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import MainNav from './components/main-nav/main-nav'
-import Logo from './components/logo/logo'
 
-const logo = new Logo('.main-header__logo')
-const nav = new MainNav('.main-nav')
+const nav = new MainNav('.main-header')
