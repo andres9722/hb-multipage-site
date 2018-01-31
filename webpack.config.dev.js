7841c1c3521757e25d1993e253fa34be84e8a66d
andres9722 <andres_9722@outlook.com>
2018-01-31 11:42:43 -0500

    Adding webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -31,7 +31,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: 'babel-loader'
       },
